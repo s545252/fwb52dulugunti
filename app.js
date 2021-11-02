a38c8a7728fbc0371e07fc7a92a84d2d954511db
s545252 <s545252@nwmissouri.edu>
2021-11-01 21:25:04 -0500

    Added Selector file with images

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var roseRouter = require('./routes/rose');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -24,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/rose', roseRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
